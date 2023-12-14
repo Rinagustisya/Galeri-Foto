6e48aca1229595620839b594b5debbe353aebf4d
Rinagustisya <rinaagustisya885@gmail.com>
2023-12-14 11:43:37 +0700

    style : landing page

--- Software/GaleriFoto/resources/views/welcome.blade.php
+++ Software/GaleriFoto/resources/views/welcome.blade.php
@@ -19,16 +19,77 @@
             <div class="container border p-3">
                 <div class="row">
                     <div class="col-12">
-                    ini contojh
+                        <div class="row first-row justify-content-center">
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Arsitektur</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Dokumenter</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Seni Rupa</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Fashion</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Olahraga</span>
+                            </div>
+                            </div>
                     </div>
+                    <br>
                     <div class="col-12">
-                        sama
+                        <div class="row justify-content-center">
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Makanan</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Satwa Liar</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Hewan</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Laut</span>
+                            </div>
+
+                            <div class="col-2">
+                                <button><i class="fas fa-bars"></i></button>
+                                <span class="label">Perjalanan</span>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- end kategori -->
+
+    <!-- Foto Terbaru -->
+    <div class="card">
+        <div class="card-body">
+            <h3><strong>Foto Terbaru</strong></h3>
+            <div class="container border p-3">
+            </div>
+        </div>
+    </div>
+    <!-- end foto -->
 
 </div>
 @endsection
