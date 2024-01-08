916e844b935117bd7b147c4879460625ea86ad7d
Rinagustisya <rinaagustisya885@gmail.com>
2024-01-08 08:52:51 +0700

    feat : profile page

--- Software/GaleriFoto/resources/views/profile.blade.php
+++ Software/GaleriFoto/resources/views/profile.blade.php
@@ -2,10 +2,76 @@
 
 @section('content')
 <div class="jumbotron">
-   <div class="card">
-        <div class="card-body">
-            
-        </div>
-   </div>
+   <h4><b>Profil</b></h4>
+   <form action="" method="post" enctype="multipart/form-data">
+      <div class="card">
+         <div class="card-body">
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Nama Lengkap</label>
+                              <input type="text" name="nama_lengkap" class="form-control" id="" value="" placeholder="Nama Lengkap" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 1 -->
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Username</label>
+                              <input type="text" name="username" class="form-control" id="" value="" placeholder="Username" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 2 -->
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Email</label>
+                              <input type="text" name="email" class="form-control" id="" value="" placeholder="email" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 3 -->
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Alamat</label>
+                              <input type="text" name="alamat" class="form-control" id="" value="" placeholder="Alamat" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 4 -->
+                  <button type="submit" class="btn btn-primary btn-block">Ubah Profil</button>
+         </div>
+      </div>
+   </form>
+
+   <h4><b>Edit Password</b></h4>
+   <form action="" method="post" enctype="multipart/form-data">
+      <div class="card">
+         <div class="card-body">
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Password</label>
+                              <input type="password" name="password" class="form-control" id="" value=""  placeholder="Masukan Password" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 1 -->
+                  <div class="card col-12">
+                     <div class="row">
+                        <div class="card-body">
+                              <label for="">Konfirmasi Password</label>
+                              <input type="password" name="password" class="form-control" id="" value=""  placeholder="Konfirmasikan Password" >
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end input 2 -->
+                  <button type="submit" class="btn btn-primary btn-block">Ubah Password</button>
+         </div>
+      </div>
+   </form>
 </div>
 @endsection
