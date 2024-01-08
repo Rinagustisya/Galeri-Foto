9ab15abf06a44aea3598fb8bb0a826788c11855c
Rinagustisya <rinaagustisya885@gmail.com>
2024-01-08 09:30:19 +0700

    feat : munculkan value

--- Software/GaleriFoto/resources/views/profile.blade.php
+++ Software/GaleriFoto/resources/views/profile.blade.php
@@ -10,7 +10,7 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Nama Lengkap</label>
-                              <input type="text" name="nama_lengkap" class="form-control" id="" value="" placeholder="Nama Lengkap" >
+                              <input type="text" name="nama_lengkap" class="form-control" id="" value="{{ Auth::user()->nama_lengkap }}" placeholder="Nama Lengkap" >
                         </div>
                      </div>
                   </div>
@@ -19,7 +19,7 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Username</label>
-                              <input type="text" name="username" class="form-control" id="" value="" placeholder="Username" >
+                              <input type="text" name="username" class="form-control" id="" value="{{ Auth::user()->username }}" placeholder="Username" >
                         </div>
                      </div>
                   </div>
@@ -28,7 +28,7 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Email</label>
-                              <input type="text" name="email" class="form-control" id="" value="" placeholder="email" >
+                              <input type="text" name="email" class="form-control" id="" value="{{ Auth::user()->email }}" placeholder="email" >
                         </div>
                      </div>
                   </div>
@@ -37,7 +37,7 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Alamat</label>
-                              <input type="text" name="alamat" class="form-control" id="" value="" placeholder="Alamat" >
+                              <input type="text" name="alamat" class="form-control" id="" value="{{ Auth::user()->alamat }}" placeholder="Alamat" >
                         </div>
                      </div>
                   </div>
@@ -55,7 +55,7 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Password</label>
-                              <input type="password" name="password" class="form-control" id="" value=""  placeholder="Masukan Password" >
+                              <input type="password" name="password" class="form-control" id="" placeholder="Masukan Password Baru" >
                         </div>
                      </div>
                   </div>
@@ -64,12 +64,12 @@
                      <div class="row">
                         <div class="card-body">
                               <label for="">Konfirmasi Password</label>
-                              <input type="password" name="password" class="form-control" id="" value=""  placeholder="Konfirmasikan Password" >
+                              <input type="password" name="password_confirmation" class="form-control" id="" placeholder="Konfirmasikan Password" >
                         </div>
                      </div>
                   </div>
                   <!-- end input 2 -->
-                  <button type="submit" class="btn btn-primary btn-block">Ubah Password</button>
+                  <button type="submit" class="btn btn-danger btn-block">Ubah Password</button>
          </div>
       </div>
    </form>
