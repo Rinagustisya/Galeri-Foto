a4087450742f44be789586e06199fa623e19f551
Rinagustisya <rinaagustisya885@gmail.com>
2024-01-15 10:03:07 +0700

    to do : style kategori

--- Software/GaleriFoto/resources/views/dashboard.blade.php
+++ Software/GaleriFoto/resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="jumbotron">
-   <div class="card" style="background-color: #ecf0f1; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+   <div class="card align-items-center justify-content-center" style="background-color: #ecf0f1; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
         <div class="card-body">
             <div style="background-color: #3498db; height: 5px; width: 50px; margin-bottom: 10px;"></div>
             <h3 style="color: black; font-weight: bold;">Selamat Datang, {{ Auth::user()->nama_lengkap }}</h3>
--- Software/GaleriFoto/resources/views/layouts/navbar.blade.php
+++ Software/GaleriFoto/resources/views/layouts/navbar.blade.php
@@ -11,8 +11,8 @@
                 @auth
                 <x-nav-item label="Dashboard" :link="route('dashboard')" />
                     <x-nav-item label="Beranda" :link="route('home')" />
-                    <x-nav-item label="Profile" :link="route('profile')" />
                     <x-nav-item label="Data Gambar" :link="route('data-foto')" />
+                    <x-nav-item label="Profile" :link="route('profile')" />
                     <x-nav-item label="Logout" :link="route('logout')" />
                 @else
                     <x-nav-item label="Login" :link="route('login.show')" />
