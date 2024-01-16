a6a8275bc5f8a8c5a2edfeddc7da41a12b6dca2a
Rinagustisya <rinaagustisya885@gmail.com>
2024-01-16 23:07:16 +0700

    ref : use storage

--- Software/GaleriFoto/app/Http/Controllers/DataFotoController.php
+++ Software/GaleriFoto/app/Http/Controllers/DataFotoController.php
@@ -7,6 +7,7 @@ use App\Models\Album;
 use App\Models\Foto;
 use Alert;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class DataFotoController extends Controller
 {
